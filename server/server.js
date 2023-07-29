cdd300c5ff14ad0ef7f35c2120cd4a0211b6f44d
shru <shruthishekar@gmail.com>
2023-07-29 12:34:03 -0600

    modified server.js

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ const startApolloServer = async () => {
   }
 
   //app.use(routes);
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
 
